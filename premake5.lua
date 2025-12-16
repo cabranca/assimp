b0067532f7b94aa101ee9314b19fb68154937853
cabranca <joaquincabrera98@gmail.com>
2025-12-16 03:10:54 -0300

    Update premake to compile on Windows

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,7 @@ project "Assimp"
     language "C++"
     cppdialect "C++20"
     staticruntime "on"
+    warnings "Off"
 
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
@@ -15,45 +16,32 @@ project "Assimp"
         "include/**.hpp",
         "include/**.inl",
         
-        -- Dependencias internas necesarias (Zlib, Unzip, etc)
-        -- Si ya tenés zlib en tu engine por otro lado, podés quitar esto y linkearlo.
-        -- "contrib/zlib/**.h",
-        -- "contrib/zlib/**.c",
-        -- "contrib/zlib/**.cpp",
         "contrib/unzip/**.h",
         "contrib/unzip/**.c",
+
         "contrib/irrXML/**.h",
         "contrib/irrXML/**.cpp",
         "contrib/pugixml/src/**.hpp",
         "contrib/pugixml/src/**.cpp",
         "contrib/utf8cpp/source/**.h",
+        
         "contrib/openddlparser/code/**.cpp",
+        "contrib/openddlparser/code/**.h",
     }
 
     removefiles
     {
-        -- Excluir pruebas y herramientas
         "code/Tests/**",
-        "code/Pbrt/**", -- A veces da problemas si no se configura bien
+        "code/Pbrt/**",
         "test/**",
         "tools/**",
         "samples/**",
 
-        -- Excluir toda la lógica de exportación (Exporters)
         "code/**/*Exporter.cpp",
         "code/**/*Exporter.h",
         "code/**/*Export*",
         
-        -- Opcional: A veces PostProcessing también tiene archivos que sobran
         "code/PostProcessing/*Exporter*",
-
-        -- Excluir wrappers de C++ viejos y otros ejemplos de zlib que no necesitamos
-        "contrib/zlib/contrib/iostream/**",
-        "contrib/zlib/contrib/iostream2/**",
-        "contrib/zlib/contrib/ada/**",
-        "contrib/zlib/contrib/delphi/**",
-        "contrib/zlib/contrib/dotzlib/**",
-        "contrib/zlib/contrib/pascal/**",
     }
 
     includedirs
@@ -72,23 +60,18 @@ project "Assimp"
 
     defines
     {
-        -- Definiciones para asegurar que Assimp compile estáticamente
-        "ASSIMP_BUILD_NO_OWN_ZLIB=0", 
-        "_CRT_SECURE_NO_WARNINGS" -- Para compatibilidad si compilás en Windows luego
+        "ASSIMP_BUILD_NO_EXPORT",
+        "ASSIMP_BUILD_NO_M3D_IMPORTER", 
+        "ASSIMP_BUILD_NO_C4D_IMPORTER", 
+        "ASSIMP_BUILD_NO_IFC_IMPORTER",
+        "ASSIMP_BUILD_NO_USD_IMPORTER",
+        "ASSIMP_BUILD_NO_VRML_IMPORTER",
+        "_CRT_SECURE_NO_WARNINGS",
     }
 
     filter "system:linux"
         pic "On"
         systemversion "latest"
-        defines 
-        { 
-            "ASSIMP_BUILD_NO_EXPORT",
-            "ASSIMP_BUILD_NO_M3D_IMPORTER", 
-            "ASSIMP_BUILD_NO_C4D_IMPORTER", 
-            "ASSIMP_BUILD_NO_IFC_IMPORTER",
-            "ASSIMP_BUILD_NO_USD_IMPORTER",
-            "ASSIMP_BUILD_NO_VRML_IMPORTER",
-        } -- A veces el importador M3D falla en GCC sin configuración extra
 
         removefiles
         {
@@ -102,7 +85,19 @@ project "Assimp"
 
     filter "system:windows"
         systemversion "latest"
-        defines { "WIN32_LEAN_AND_MEAN" }
+        defines { "WIN32_LEAN_AND_MEAN", "OPENDDL_STATIC_LIBARY", }
+
+        files
+        {
+            "contrib/zlib/**.h",
+            "contrib/zlib/**.c",
+            "contrib/zlib/**.cpp",
+        }
+
+        removefiles
+        {
+            "contrib/zlib/contrib/**",
+        }
 
     filter "configurations:Debug"
         runtime "Debug"
